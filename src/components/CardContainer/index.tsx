1179ee57c245d6b119e7e1e74aff9077f0bb65ae
Connor Hudson <cohu3291@colorado.edu>
2018-02-25 00:16:29 -0700

    cardcontainer initial display

--- src/components/CardContainer/index.tsx
+++ src/components/CardContainer/index.tsx
@@ -1,17 +1,21 @@
 import * as React from 'react';
 import './index.css';
-import Card from '../Card';
+import { Charity } from '../../objects';
+import CharityCard from '../CharityCard';
 
 class CardContainer extends React.Component {
+  charities: Charity[] = [
+    {name: 'WWF', description: 'animal help'},
+    {name: 'EthanCo', description: 'Ethan\'s Evil Deeds'},
+    {name: 'MyTuition', description: 'I\'m a broke college student, pls halp'},
+    {name: 'EFF', description: 'The Electronic Frontier Foundation is the leading nonprofit organization defending civil liberties in the digital world.'},
+  ]
   render() {
     return (
       <div className="CardContainer">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {this.charities.map((charity) => (
+          <CharityCard charity={charity} />
+        ))}
       </div>
     );
   }
