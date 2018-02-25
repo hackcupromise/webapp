bdf79e6326b85cca6fb7749251d972e5a1c296d5
Connor Hudson <cohu3291@colorado.edu>
2018-02-24 22:28:10 -0700

    Add CardContainer component implementation

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react'
 import Sidebar from '../Sidebar'
+import CardContainer from '../CardContainer'
+
 import './index.css'
 
 class App extends React.Component {
@@ -7,6 +9,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <Sidebar />
+        <CardContainer />
       </div>
     );
   }
---
+++ src/components/Card/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import './index.css';
+
+class Card extends React.Component {
+  render() {
+    return (
+      <div className="Card" />
+    );
+  }
+}
+
+export default Card;
