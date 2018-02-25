b78f9b673926788469df31776fe25bbb28f8198d
Connor Hudson <cohu3291@colorado.edu>
2018-02-25 00:16:44 -0700

    Add optional classname to card

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,14 +1,15 @@
 import * as React from 'react';
 import './index.css';
 
-interface Props {
-  children?: React.Component[]
+interface Props extends React.Props<void> {
+  children?: any
+  className?: string
 }
 
-class Card extends React.Component<Props> {
+class Card extends React.Component<Props, {}> {
   render() {
     return (
-      <div className="Card">
+      <div className={`Card ${this.props.className}`}>
         {this.props.children}
       </div>
     );
