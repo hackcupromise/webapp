3d45de09d343f784e353de717d9dd2ff2a381398
Connor Hudson <cohu3291@colorado.edu>
2018-02-24 19:28:52 -0700

    Move App to components folder

---
+++ src/components/App/index.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react'
+import Sidebar from '../Sidebar'
+import './index.css'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Sidebar />
+      </div>
+    );
+  }
+}
+
+export default App;
