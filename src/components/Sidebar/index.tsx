4001f2261ffb3dbdbbe9fbc28d0cb6ce69d39ff9
Connor Hudson <cohu3291@colorado.edu>
2018-02-24 19:31:47 -0700

    Add basic sidebar layout

---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import './index.css';
+
+class Sidebar extends React.Component {
+  render() {
+    return (
+      <div className="Sidebar" />
+    );
+  }
+}
+
+export default Sidebar;
